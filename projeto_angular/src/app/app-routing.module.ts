570d67f0e13b246307ae4d2b2596044f3a01094e
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-05-12 14:02:01 -0300

    Add Routes in Project

--- projeto_angular/src/app/app-routing.module.ts
+++ projeto_angular/src/app/app-routing.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UsuarioComponent } from './usuario/usuario.component';
+
+
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'usuario', component: UsuarioComponent},
-  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(x => x.HomeModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then(x => x.UsuarioModule)
+
+  },
+  // {
+  //   path: 'estados',
+  //   loadChildren: () => import('.').then(x => x.CustomerAppSupplierModule)
+  // },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/home'
+  }
+
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash:true})
   ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
-
-
-
